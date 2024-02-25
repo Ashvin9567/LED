f63d840133cb3916941b2da8ba6acbdce55e4541
Ashvin9567 <44165241+Ashvin9567@users.noreply.github.com>
2024-02-25 16:29:59 +0000

    Add files via upload

---
+++ LED_blink_without_delay.ino
@@ -0,0 +1,44 @@
+// LED blink without delay
+
+// Constant (unchanging)
+  const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
+// Variable (will change)
+  int ledState = LOW;  // ledState used to set the LED
+
+  unsigned long previousMillis = 0;  // will store last time LED was updated
+  // (Generally, you should use "unsigned long" for variables that hold time.
+  // The value will quickly become too large for an int to store)
+
+// Constant
+  const long interval = 1000;  // interval at which to blink (milliseconds)
+
+// Setup (runs once)
+  void setup() {
+    // set the digital pin as output:
+    pinMode(ledPin, OUTPUT);
+  }
+
+// Main loop (repeats)
+  void loop() {
+    // check to see if it's time to blink the LED (if the difference
+    // between the current time and last time you blinked the LED is bigger than
+    // the interval at which you want to blink the LED).
+
+    unsigned long currentMillis = millis(); // gets current time
+
+    if (currentMillis - previousMillis >= interval) {
+      // save the last time you blinked the LED
+      previousMillis = currentMillis;       // turns current time into "previous" for referal
+
+      // if the LED is off turn it on and vice-versa:
+      if (ledState == LOW) {
+        ledState = HIGH;
+      } else {
+        ledState = LOW;
+      }
+
+      // set the LED with the ledState of the variable:
+      digitalWrite(ledPin, ledState);
+    }
+  }
